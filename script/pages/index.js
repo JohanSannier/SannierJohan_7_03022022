dd437789fc7efe58f53b55cc3cb7216f0c490ad1
Johan <johan.sanniercaffier@gmail.com>
2022-02-11 09:45:02 +0100

    WIP filtres container

--- script/pages/index.js
+++ script/pages/index.js
@@ -33,15 +33,21 @@ function getInputFilters(type, color) {
   input.classList.add('btn', `btn-${color}`);
   // input.style.width = '100px';
   const dataName = type.getAttribute('data-name');
-  console.log(dataName);
   input.setAttribute('placeholder', `Rechercher un ${dataName}`);
+  input.classList.add('text-white');
   type.innerText = '';
   type.appendChild(input);
 }
 
-async function createContainerFilter(parent, type) {
+async function createContainerFilter(parent, type, color) {
   let container = document.createElement('div');
+  let row = document.createElement('div');
+  row.classList.add('row');
+
+  container.classList.add(`bg-${color}`);
   let list = document.createElement('ul');
+  list.classList.add('main-ul');
+  list.classList.add('list-unstyled');
   const recipes = await getRecipes();
 
   recipes.forEach(async (recipe) => {
@@ -55,15 +61,14 @@ async function createContainerFilter(parent, type) {
   });
 
   const array = createFilteredArray(type);
-  console.log(array);
   array.forEach((element) => {
     let liste = document.createElement('li');
+    liste.classList.add('main-list');
     liste.innerText = element;
     list.appendChild(liste);
   });
   parent.appendChild(container);
   container.appendChild(list);
-  createListItem(type);
 }
 
 // Création de la fonction qui récupère les ingrédients, appareils et ustensils et les classe dans un nouveau tableau sans doublon
@@ -95,23 +100,21 @@ window.addEventListener('click', (e) => {
   switch (e.target.id) {
     case 'ingredients':
       getInputFilters(e.target, 'primary');
-      createContainerFilter(colBtnIngredient, e.target.id);
+      createContainerFilter(colBtnIngredient, e.target.id, 'primary');
       break;
     case 'appliance':
       getInputFilters(e.target, 'success');
-      createContainerFilter(colBtnAppliance, e.target.id);
+      createContainerFilter(colBtnAppliance, e.target.id, 'success');
       break;
     case 'ustensil':
       getInputFilters(e.target, 'danger');
-      createContainerFilter(colBtnUstensil, e.target.id);
+      createContainerFilter(colBtnUstensil, e.target.id, 'danger');
       break;
 
     default:
       break;
   }
 });
-
-function createListItem(type) {}
 
 // Init
 displayRecipes();
